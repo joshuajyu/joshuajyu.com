a326d80832dde64d1f46d3003c5b87cf98c60c47
Joshua Yu <jo2004yu@gmail.com>
2024-12-17 00:34:42 -0500

    styling change

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 
 export function Header() {
   return (
-    <div className="flex flex-row items-baseline justify-between py-7 px-10 sm:px-10 md:px-20 lg:px-32 xl:px-60">
+    <div className="flex flex-row items-baseline justify-between py-10 px-10 sm:px-10 md:px-20 lg:px-32 xl:px-60">
       <NavMenu />
       <div className="flex flex-row items-center gap-6">
         <Button variant="link" className="p-0" asChild>
