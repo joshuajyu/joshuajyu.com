beefd0b3241c161b5e6d8f9ec2582da42b0cbddc
Joshua Yu <jo2004yu@gmail.com>
2024-12-17 00:34:24 -0500

    aria change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <h2 className="scroll-m-20 text-4xl font-semibold tracking-tight pb-4 pt-10">
-        Hello! 👋
+        Hello! <span aria-hidden="true">👋</span>
       </h2>
       <p>
         I'm Joshua Yu, a student studying Computer Science at the University of Toronto Scarborough.
