6380e0869af3ee7f5ad15f08823e1cd27e2e9c8f
Joshua Yu <jo2004yu@gmail.com>
2024-12-17 15:07:33 -0500

    add dynamic favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,27 @@
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Header } from "@/components/header";
-import "./globals.css";
 import { Footer } from "@/components/footer";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Joshua Yu | Computer Science Student @ UofT Scarborough",
   description:
     "Joshua Yu is a Computer Science student at the University of Toronto Scarborough, passionate about building software and exploring new technologies.",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme:dark)",
+        url: "images/favicon-dark.ico",
+        href: "images/favicon-dark.ico",
+      },
+      {
+        media: "(prefers-color-scheme:light)",
+        url: "images/favicon-light.ico",
+        href: "images/favicon-light.ico",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
