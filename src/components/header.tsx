34c95f2603908598d41c57805e6f779120d99f27
Joshua Yu <jo2004yu@gmail.com>
2024-12-17 15:07:51 -0500

    update padding

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,11 +3,10 @@ import { NavMenu } from "./nav-menu";
 import { ModeToggle } from "./mode-toggle";
 import { Button } from "./ui/button";
 import { ExternalLink } from "lucide-react";
-import Image from "next/image";
 
 export function Header() {
   return (
-    <div className="flex flex-row items-baseline justify-between py-10 px-10 sm:px-10 md:px-20 lg:px-32 xl:px-60">
+    <div className="flex flex-row items-baseline justify-between pt-10 px-10 sm:px-10 md:px-20 lg:px-32 xl:px-60 pb-8 lg:pb-14">
       <NavMenu />
       <div className="flex flex-row items-center gap-6">
         <Button variant="link" className="p-0" asChild>
