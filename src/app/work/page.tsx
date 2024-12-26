960bae33fb576189604a9a3570f0149f367dd9d2
Joshua Yu <jo2004yu@gmail.com>
2024-12-26 18:18:17 -0500

    update work page

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,3 +1,5 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { TriangleAlert } from "lucide-react";
 import { CardTable } from "@/components/work-library";
 
 export default function Work() {
@@ -6,10 +8,15 @@ export default function Work() {
       <h2 className="scroll-m-20 text-4xl font-semibold tracking-tight pb-4">
         Work
       </h2>
-      <p>
-        Here is a collection of some of my work, with links to their GitHub repositories.
-      </p>
       <CardTable />
+      <Alert className="hidden sm:block">
+        <TriangleAlert size={16} />
+        <AlertTitle className="font-bold">Tip:</AlertTitle>
+        <AlertDescription>
+          If you don't know what a particular icon is, hover over it for a
+          clarification.
+        </AlertDescription>
+      </Alert>
       <blockquote className="mt-6 border-l-2 pl-6 italic text-muted-foreground">
         “Far and away the best prize that life has to offer is the chance to
         work hard at work worth doing.”
--- src/components/work-library.tsx
+++ src/components/work-library.tsx
@@ -5,64 +5,117 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { TypeScript, NextJS, AWS, GoogleCloud } from "./icons/devicons";
-import { ExternalLink } from "lucide-react";
+} from "./ui/card";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+import {
+  TypeScript,
+  NextJS,
+  AWS,
+  GoogleCloud,
+  Google,
+  JavaScript,
+  ReactIcon,
+  MongoDB,
+  OpenAI,
+  Python,
+  GitHub,
+} from "./icons/devicons";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "./ui/button";
+import { Link } from "lucide-react";
 
 const works: {
-  name: JSX.Element;
-  icons: JSX.Element[];
+  name: string;
+  icons: { icon: JSX.Element; tooltip: string }[];
   description: JSX.Element;
+  link: string;
 }[] = [
   {
-    name: (
-      <a
-        href="https://github.com/joshuajyu/joshuajyu.com"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        <span className="underline-offset-2 hover:underline">
-          joshuajyu.com
-        </span>{" "}
-        <ExternalLink size={14} className="inline" />
-      </a>
-    ),
+    name: "LangChain.js",
     icons: [
-      <TypeScript width={20} height={20} />,
-      <NextJS width={20} height={20} />,
-      <AWS width={20} height={20} />,
+      { icon: <TypeScript />, tooltip: "TypeScript" },
+      { icon: <Google />, tooltip: "SerpAPI" },
+      { icon: <GitHub />, tooltip: "GitHub" },
     ],
-    description: <p>My personal website&mdash;the one you're reading this on!</p>,
+    description: (
+      <p>
+        A Google Finance integration developed for the LangChain.js library.
+      </p>
+    ),
+    link: "https://github.com/langchain-ai/langchainjs/pull/7289",
   },
   {
-    name: (
-      <a
-        href="https://github.com/joshuajyu/utscdle"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        <span className="underline-offset-2 hover:underline">utscdle</span>{" "}
-        <ExternalLink size={14} className="inline" />
-      </a>
-    ),
+    name: "joshuajyu.com",
     icons: [
-      <TypeScript width={20} height={20} />,
-      <NextJS width={20} height={20} />,
-      <GoogleCloud width={20} height={20} />,
+      { icon: <TypeScript />, tooltip: "TypeScript" },
+      { icon: <NextJS />, tooltip: "Next.js" },
+      { icon: <AWS />, tooltip: "Amazon EC2" },
+    ],
+    description: (
+      <p>My personal website&mdash;the one you're reading this on!</p>
+    ),
+    link: "https://github.com/joshuajyu/joshuajyu.com",
+  },
+  {
+    name: "UTSCdle",
+    icons: [
+      { icon: <TypeScript />, tooltip: "TypeScript" },
+      { icon: <NextJS />, tooltip: "Next.js" },
+      { icon: <GoogleCloud />, tooltip: "Google Cloud Platform" },
+      { icon: <MongoDB />, tooltip: "MongoDB" },
     ],
     description: (
       <p>A Wordle-inspired UofT Scarborough (UTSC) daily guessing game.</p>
     ),
+    link: "https://github.com/joshuajyu/utscdle",
+  },
+  {
+    name: "Pactify",
+    icons: [
+      { icon: <JavaScript />, tooltip: "JavaScript" },
+      { icon: <ReactIcon />, tooltip: "React" },
+      { icon: <OpenAI />, tooltip: "OpenAI API" },
+      { icon: <AWS />, tooltip: "Amazon S3" },
+      { icon: <MongoDB />, tooltip: "MongoDB" },
+    ],
+    description: (
+      <p>
+        A free AI contract generator that allows users to generate contracts
+        from predefined templates.
+      </p>
+    ),
+    link: "https://github.com/joshuajyu/utscdle",
+  },
+  {
+    name: "Discribe",
+    icons: [
+      { icon: <Python />, tooltip: "Python" },
+      { icon: <JavaScript />, tooltip: "JavaScript" },
+      { icon: <OpenAI />, tooltip: "OpenAI Cohere" },
+    ],
+    description: (
+      <p>
+        A Discord bot that records, transcribes, and summarizes voice calls.
+        Developed in 36 hours @ Hack the Valley 7.
+      </p>
+    ),
+    link: "https://github.com/joshuajyu/utscdle",
   },
 ];
 
 export function CardTable() {
   return (
-    <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {works.map((component, index) => (
         <WorkCard
           key={index}
           name={component.name}
+          link={component.link}
           icons={component.icons}
           description={component.description}
         />
@@ -74,24 +127,45 @@ export function CardTable() {
 const WorkCard = React.forwardRef<
   React.ElementRef<"div">,
   React.ComponentPropsWithoutRef<"div"> & {
-    name: JSX.Element;
-    icons: JSX.Element[];
+    name: string;
+    icons: { icon: JSX.Element; tooltip: string }[];
     description: JSX.Element;
+    link: string;
   }
->(({ name, icons, description }, ref) => {
+>(({ name, icons, description, link }, ref) => {
   return (
-    <Card ref={ref}>
-      <CardHeader>
-        <CardTitle>{name}</CardTitle>
-        {/* <CardDescription className="flex flex-row items-center gap-1">
-          {icons.map((icon) => icon)}
-        </CardDescription> */}
-      </CardHeader>
-      <CardContent>
-        <p>{description}</p>
-      </CardContent>
-      <CardFooter className="self-end flex flex-row items-center gap-2">
-        {icons.map((icon) => icon)}
+    <Card ref={ref} className="flex flex-col justify-between items-start">
+      <div aria-hidden="true" className="w-full">
+        <CardHeader className="pb-3">
+          <CardTitle className="flex flex-row justify-between items-center">
+            <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+              {name}
+            </h4>
+            <a
+              href={link}
+              rel="noopener noreferrer"
+              target="_blank"
+              className={cn(buttonVariants({ variant: "ghost", size: "icon" }))}
+            >
+              <Link />
+            </a>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>{description}</CardContent>
+      </div>
+      <CardFooter className="flex flex-row items-center gap-2">
+        {icons.map((element) => (
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                {React.cloneElement(element.icon, { width: 20, height: 20 })}
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{element.tooltip}</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        ))}
       </CardFooter>
     </Card>
   );
